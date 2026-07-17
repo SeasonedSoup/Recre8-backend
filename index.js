f3369c4681314589f7f778039151e5b3942ec23c
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-07-17 17:25:52 +0800

    Add test server

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send("HI IM RECRE-8 API");
+})
+
+app.listen(PORT, (err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log(`The server is now listening at http://localhost:${PORT}`);
+})
