a8b0e631e93a607ea53974e89c2655ccf39b54eb
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-07-17 18:15:33 +0800

    Finish add firned and plan to accept friend func

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -1,3 +1,18 @@
-addFriend = async(req, res) => {
+const { prisma } = require("../lib/prisma")
 
+addFriend = async(req, res) => {
+    const result = await prisma.friend.create({
+        data: {
+            userId: req.user.userId,
+            friendId: req.body.friendId
+        }
+    })
+    res.status(200).json(result)
+}
+
+acceptFriend = async(req, res) => {
+    const result = await prisma.friend.findUnique({
+        data: {
+    })
+    res.status(200).json(result)
 }
